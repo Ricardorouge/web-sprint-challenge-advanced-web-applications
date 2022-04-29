1c7270455d79c62a022b592eaa4c655aeab78618
Ricardorouge <ricky032929@gmail.com>
2022-04-29 11:27:59 -0400

    axioswithAuth

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,13 @@
 // ✨ implement axiosWithAuth
+import axios from "axios";
+
+const axiosWithAuth = ()=>{
+    axios.create({
+        headers:{
+            authorization:localStorage.getItem('token')
+        },
+        baseURL:'http://localhost:9000/api'
+    })
+}
+
+export default axiosWithAuth
